69651a6cbcda9637588f3a7b4a02768c09a5400d
Emmanuel Ardoin <emmanuel.ardoin@ypok.com>
2024-11-06 16:29:40 +0100

    oui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { DleChampionsTitleComponent } from './dle-champions-title/dle-champions-title.component';
-import { AcceuilComponent } from './acceuil/acceuil.component';
 
 export const routes: Routes = [
     { path: '', component: DleChampionsTitleComponent },
